1f543a776f1a5f258b6827d966c93cd59d10b169
Pedram Valiani <p.valiani@gmail.com>
2021-02-04 13:37:56 +0000

    setting up player

---
+++ src/main/java/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+
+    private String selectedPlayer;
+    private String name;
+    private int healthPoints;
+
+
+    public Player(String selectedPlayer, String name, int healthPoints){
+
+       this.selectedPlayer =  selectedPlayer;
+       this.name =  name;
+       this.healthPoints = healthPoints;
+    }
+
+
+}
