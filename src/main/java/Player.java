4ca46306ab5a321939b5d2944c5e1813ab2f230d
Pedram Valiani <p.valiani@gmail.com>
2021-02-04 13:39:14 +0000

    set up getters and setters for player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,9 +1,10 @@
-public class Player {
+public abstract class Player {
 
     private String selectedPlayer;
     private String name;
     private int healthPoints;
 
+//    player constructor class - takes in a name, player selection i.e barbarian and healthPoints
 
     public Player(String selectedPlayer, String name, int healthPoints){
 
@@ -11,6 +12,33 @@ public class Player {
        this.name =  name;
        this.healthPoints = healthPoints;
     }
+
+//    getters and setters for selectedPlayer
+
+    public String getSelectedPlayer() {
+        return selectedPlayer;
+    }
+
+    public void setSelectedPlayer(String selectedPlayer) {
+        this.selectedPlayer = selectedPlayer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
 
 
 }
