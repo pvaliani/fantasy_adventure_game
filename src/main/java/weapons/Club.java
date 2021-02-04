6b3f8bebddab4e180c927fae30ec25957322c08d
Pedram Valiani <p.valiani@gmail.com>
2021-02-04 15:59:32 +0000

    club can deal damage

--- src/main/java/weapons/Club.java
+++ src/main/java/weapons/Club.java
@@ -2,10 +2,26 @@ package weapons;
 
 import enemies.Enemy;
 import players.IWeapon;
+import players.Player;
+
 
 public class Club implements IWeapon {
 
+    private int damageValue;
+
+//    deal 20 damage i.e negative health points when there is an attack on
+
+    public Club(int damageValue){
+        this.damageValue = -20;
+    }
+
+//    attack method from IWeapon interface
+
         public void attack(Enemy enemy){
 
+            enemy.setHealthPoints(this.damageValue);
+
     }
+
+
 }
